0e29c0ea881ad8abf3b6f8175e8e6eae8b7aa1a5
theboogieferret <166142774+theboogieferret@users.noreply.github.com>
2025-01-23 10:50:20 +0200

    push 2

--- My project/Assets/GrCheck.cs
+++ My project/Assets/GrCheck.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class GrCheck : MonoBehaviour
 {
     // Start is called before the first frame update
+    PlayerController controller;
     
     void Start()
     {
-        
+        controller=FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer==6)
+        {
+            controller.CJump();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            controller.NoJump();
+        }
     }
 }
--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -7,34 +7,46 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private float moveHorizontal;
     public Rigidbody2D rb2d;
-    public float speed;
+    public float speed=20;
     private bool jump=false;
-    public float jumpSpeed;
+    public float jumpSpeed=10;
+    private bool grounded=false;
     void Start()
     {
         rb2d.freezeRotation = true;
         FindObjectOfType<GrCheck>();
+        speed = 10;
+        jumpSpeed = 10;
+    }
+    public void CJump()
+    {
+        grounded = true;
+    }
+
+    public void NoJump()
+    {
+        grounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(grounded);
         moveHorizontal = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&grounded)
         {
             jump = true;
         }
-
+        //Debug.Log(CanJump);
     }
 
     void FixedUpdate()
     {
         rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
-
-        if (jump)
+        if (jump&& grounded)
         {
-            Debug.Log("jump");
             jump = false;
+            grounded = false;
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
         }
     }
